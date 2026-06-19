21fa927a6178a0c26f68040712d33310d73868a6
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-06-19 21:28:19 +0530

    ✅ [GeeksforGeeks] Missing in Array |

--- GeeksforGeeks/Medium/1/Solution.cpp
+++ GeeksforGeeks/Medium/1/Solution.cpp
@@ -1,23 +1,14 @@
 class Solution {
-    public int countIncreasing(int[] arr) {
+    int missingNum(int arr[]) {
+        int n = arr.length + 1;
         
-        int n = arr.length;
-        long count = 0;
+        long expectedSum = (long) n * (n + 1) / 2;
+        long actualSum = 0;
         
-        int len = 1;
-        
-        for (int i = 1; i < n; i++) {
-            
-            if (arr[i] > arr[i - 1]) {
-                len++;
-            } else {
-                count += (long)(len - 1) * len / 2;
-                len = 1;
-            }
+        for (int num : arr) {
+            actualSum += num;
         }
         
-        count += (long)(len - 1) * len / 2;
-        
-        return (int) count;
+        return (int) (expectedSum - actualSum);
     }
 }

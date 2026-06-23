3b472ff14289c1dcfe9d4ab2693b8eaf7f063613
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-06-23 07:27:34 +0530

    ✅ [Easy] #0941 - Sort Array By Parity | Java | Runtime: 1 ms | Memory: 47 MB

--- Easy/0941-sort-array-by-parity/Solution.java
+++ Easy/0941-sort-array-by-parity/Solution.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
+        int[] ans = new int[nums.length];
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right) {
-            if (nums[left] % 2 > nums[right] % 2) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-            }
-            if (nums[left] % 2 == 0) {
-                left++;
-            }
-            if (nums[right] % 2 == 1) {
-                right--;
+        for(int num : nums){
+            if(num % 2 == 0){
+                ans[left++] = num;
+            }else{
+                ans[right--] = num;
             }
         }
-        
-        return nums;
+
+        return ans;
     }
 }

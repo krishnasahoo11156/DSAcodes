137bfc1dc283164cf3029767d86e066d22f1a1a2
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-06-24 19:36:42 +0530

    ✅ [Easy] #1018 - Largest Perimeter Triangle | Java | Runtime: 11 ms | Memory: 47.7 MB

--- Easy/1018-largest-perimeter-triangle/Solution.java
+++ Easy/1018-largest-perimeter-triangle/Solution.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
-class Solution{
-    public int largestPerimeter(int[] nums){
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        for (int i = nums.length - 1; i >= 2; i--){
-            if (nums[i - 2] + nums[i - 1] > nums[i]){
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
                 return nums[i - 2] + nums[i - 1] + nums[i];
             }
         }

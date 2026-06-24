9c3e536b59e485180a07bbf454c0bccb1d0125f7
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-06-24 20:31:27 +0530

    ✅ [Easy] #1019 - Squares of a Sorted Array | Java | Runtime: 1 ms | Memory: 47.4 MB

--- Easy/1019-squares-of-a-sorted-array/Solution.java
+++ Easy/1019-squares-of-a-sorted-array/Solution.java
@@ -1,12 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = Math.abs(nums[i]);
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                ans[index] = leftSquare;
+                left++;
+            } else {
+                ans[index] = rightSquare;
+                right--;
+            }
+            index--;
         }
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = nums[i] * nums[i];
-        }
-        return nums;
+
+        return ans;
     }
 }

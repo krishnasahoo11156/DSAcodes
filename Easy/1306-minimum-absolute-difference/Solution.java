196280f9897dc58d6aa5ecdfe0bb4fa52a0d1e70
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-06-25 22:49:28 +0530

    ✅ [Easy] #1306 - Minimum Absolute Difference | Java | Runtime: 21 ms | Memory: 63.9 MB

--- Easy/1306-minimum-absolute-difference/Solution.java
+++ Easy/1306-minimum-absolute-difference/Solution.java
@@ -2,16 +2,17 @@ class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
 
+        List<List<Integer>> ans = new ArrayList<>();
         int minDiff = Integer.MAX_VALUE;
 
         for (int i = 1; i < arr.length; i++) {
-            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
-        }
+            int diff = arr[i] - arr[i - 1];
 
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] == minDiff) {
+            if (diff < minDiff) {
+                minDiff = diff;
+                ans.clear();
+                ans.add(Arrays.asList(arr[i - 1], arr[i]));
+            } else if (diff == minDiff) {
                 ans.add(Arrays.asList(arr[i - 1], arr[i]));
             }
         }

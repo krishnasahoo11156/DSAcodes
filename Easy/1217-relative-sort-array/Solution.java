f532b14c91af8941adeefa00a250ea81f825effa
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-06-30 20:15:26 +0530

    ✅ [Easy] #1217 - Relative Sort Array | Java | Runtime: 5 ms | Memory: 43.8 MB

--- Easy/1217-relative-sort-array/Solution.java
+++ Easy/1217-relative-sort-array/Solution.java
@@ -1,21 +1,26 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int[] freq = new int[1001];
-        for (int num : arr1) {
-            freq[num]++;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr2.length; i++) {
+            map.put(arr2[i], i);
         }
-        int index = 0;
-        for (int num : arr2) {
-            while (freq[num] > 0) {
-                arr1[index++] = num;
-                freq[num]--;
-            }
+        Integer[] nums = new Integer[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            nums[i] = arr1[i];
         }
-        for (int i = 0; i <= 1000; i++) {
-            while (freq[i] > 0) {
-                arr1[index++] = i;
-                freq[i]--;
-            }
+        Arrays.sort(nums, (a, b) -> {
+            boolean inA = map.containsKey(a);
+            boolean inB = map.containsKey(b);
+            if (inA && inB)
+                return map.get(a) - map.get(b);
+            if (inA)
+                return -1;
+            if (inB)
+                return 1;
+            return a - b;
+        });
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = nums[i];
         }
         return arr1;
     }

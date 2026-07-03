d9ffef614fa685a65813fd1092c7bec6542e070c
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-03 16:52:56 +0530

    ✅ [Medium] #0442 - Find All Duplicates in an Array | Java | Runtime: 6 ms | Memory: 57.6 MB

--- Medium/0442-find-all-duplicates-in-an-array/Solution.java
+++ Medium/0442-find-all-duplicates-in-an-array/Solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> answer = new ArrayList<>();
-        int[] ans = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            ans[nums[i] - 1]++;
-        }
-        for(int i = 0; i < nums.length; i++){
-            if(ans[i] == 2){
-                answer.add(i+1);
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                answer.add(index + 1);
+            } else {
+                nums[index] = -nums[index];
             }
         }
         return answer;

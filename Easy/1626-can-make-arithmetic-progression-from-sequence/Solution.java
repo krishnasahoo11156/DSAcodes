37e9744181ff9a335f18d5f56a3dd4217760e3b0
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-08 09:49:15 +0530

    ✅ [Easy] #1626 - Can Make Arithmetic Progression From Sequence | Java | Runtime: 2 ms | Memory: 44.1 MB

--- Easy/1626-can-make-arithmetic-progression-from-sequence/Solution.java
+++ Easy/1626-can-make-arithmetic-progression-from-sequence/Solution.java
@@ -1,11 +1,28 @@
+import java.util.HashSet;
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i+1] - arr[i] != diff){
+        int n = arr.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int num : arr) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        if((max - min) % (n - 1) != 0)
+            return false;
+        int diff = (max - min) / (n - 1);
+        if(diff == 0)
+            return true;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : arr) {
+            if((num - min) % diff != 0)
                 return false;
-            }
+            if(!set.add(num))
+                return false;
+        }
+        for(int i = 0; i < n; i++) {
+            if(!set.contains(min + i * diff))
+                return false;
         }
         return true;
     }

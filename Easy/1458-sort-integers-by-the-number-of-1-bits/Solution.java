bbbfd7ecda70ea77b371f70e469212dcfa226161
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-10 20:54:15 +0530

    ✅ [Easy] #1458 - Sort Integers by The Number of 1 Bits | Java | Runtime: 6 ms | Memory: 46.9 MB

--- Easy/1458-sort-integers-by-the-number-of-1-bits/Solution.java
+++ Easy/1458-sort-integers-by-the-number-of-1-bits/Solution.java
@@ -1,27 +1,11 @@
 class Solution {
-    public int countBits(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
-        }
-        return count;
-    }
     public int[] sortByBits(int[] arr) {
-        Integer[] nums = new Integer[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            nums[i] = arr[i];
+            arr[i] += Integer.bitCount(arr[i]) * 100000;
         }
-        Arrays.sort(nums, (a, b) -> {
-            int bitA = countBits(a);
-            int bitB = countBits(b);
-            if (bitA == bitB) {
-                return a - b;
-            }
-            return bitA - bitB;
-        });
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = nums[i];
+            arr[i] %= 100000;
         }
         return arr;
     }

701719a2886142e50720bbb415200ecf74fdbbee
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-11 22:18:00 +0530

    ✅ [Medium] #0012 - Integer to Roman | Java | Runtime: 7 ms | Memory: 46.8 MB

--- Medium/0012-integer-to-roman/Solution.java
+++ Medium/0012-integer-to-roman/Solution.java
@@ -1,22 +1,13 @@
 class Solution {
     public String intToRoman(int num) {
-        int[] values = {
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4, 1
-        };
-        String[] symbols = {
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV", "I"
-        };
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                ans.append(symbols[i]);
-                num -= values[i];
-            }
-        }
-        return ans.toString();
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[num / 1000]
+                + hundreds[(num % 1000) / 100]
+                + tens[(num % 100) / 10]
+                + ones[num % 10];
     }
 }

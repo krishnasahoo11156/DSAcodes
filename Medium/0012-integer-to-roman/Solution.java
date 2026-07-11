2de7603a399d9e993d029b854665acab186d3139
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-11 22:20:01 +0530

    ✅ [Medium] #0012 - Integer to Roman | Java | Runtime: 3 ms | Memory: 46 MB

--- Medium/0012-integer-to-roman/Solution.java
+++ Medium/0012-integer-to-roman/Solution.java
@@ -1,13 +1,29 @@
 class Solution {
     public String intToRoman(int num) {
-        String[] thousands = {"", "M", "MM", "MMM"};
-        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-
-        return thousands[num / 1000]
-                + hundreds[(num % 1000) / 100]
-                + tens[(num % 100) / 10]
-                + ones[num % 10];
+        StringBuilder ans = new StringBuilder();
+        int thousands = num / 1000;
+        while (thousands-- > 0)
+            ans.append("M");
+        int hundreds = (num % 1000) / 100;
+        append(ans, hundreds, 'C', 'D', 'M');
+        int tens = (num % 100) / 10;
+        append(ans, tens, 'X', 'L', 'C');
+        int ones = num % 10;
+        append(ans, ones, 'I', 'V', 'X');
+        return ans.toString();
+    }
+    private void append(StringBuilder sb, int digit, char one, char five, char ten) {
+        if (digit <= 3) {
+            while (digit-- > 0)
+                sb.append(one);
+        } else if (digit == 4) {
+            sb.append(one).append(five);
+        } else if (digit <= 8) {
+            sb.append(five);
+            while (digit-- > 5)
+                sb.append(one);
+        } else {
+            sb.append(one).append(ten);
+        }
     }
 }

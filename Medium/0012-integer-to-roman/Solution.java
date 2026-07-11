8fcf3b7cc9c136648138449bce8486771968684a
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-11 22:22:53 +0530

    ✅ [Medium] #0012 - Integer to Roman | Java | Runtime: 5 ms | Memory: 46.5 MB

--- Medium/0012-integer-to-roman/Solution.java
+++ Medium/0012-integer-to-roman/Solution.java
@@ -1,29 +1,25 @@
 class Solution {
+    int[] values = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4, 1
+    };
+    String[] symbols = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    };
     public String intToRoman(int num) {
-        StringBuilder ans = new StringBuilder();
-        int thousands = num / 1000;
-        while (thousands-- > 0)
-            ans.append("M");
-        int hundreds = (num % 1000) / 100;
-        append(ans, hundreds, 'C', 'D', 'M');
-        int tens = (num % 100) / 10;
-        append(ans, tens, 'X', 'L', 'C');
-        int ones = num % 10;
-        append(ans, ones, 'I', 'V', 'X');
-        return ans.toString();
+        return solve(num);
     }
-    private void append(StringBuilder sb, int digit, char one, char five, char ten) {
-        if (digit <= 3) {
-            while (digit-- > 0)
-                sb.append(one);
-        } else if (digit == 4) {
-            sb.append(one).append(five);
-        } else if (digit <= 8) {
-            sb.append(five);
-            while (digit-- > 5)
-                sb.append(one);
-        } else {
-            sb.append(one).append(ten);
+    private String solve(int num) {
+        if (num == 0)
+            return "";
+        for (int i = 0; i < values.length; i++) {
+            if (num >= values[i]) {
+                return symbols[i] + solve(num - values[i]);
+            }
         }
+        return "";
     }
 }

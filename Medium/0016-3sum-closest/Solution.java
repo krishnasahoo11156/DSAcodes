5aeec4592757b866c2746ac798080aae89d938ed
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-13 18:37:56 +0530

    ✅ [Medium] #0016 - 3Sum Closest | Java | Runtime: 16 ms | Memory: 45.5 MB

--- Medium/0016-3sum-closest/Solution.java
+++ Medium/0016-3sum-closest/Solution.java
@@ -2,33 +2,20 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int closest = nums[0] + nums[1] + nums[2];
-        int n = nums.length;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                int required = target - nums[i] - nums[j];
-                int left = j + 1;
-                int right = n - 1;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-                    int sum = nums[i] + nums[j] + nums[mid];
-                    if (Math.abs(sum - target) < Math.abs(closest - target)) {
-                        closest = sum;
-                    }
-                    if (nums[mid] < required) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
                 }
-                if (left < n) {
-                    int sum = nums[i] + nums[j] + nums[left];
-                    if (Math.abs(sum - target) < Math.abs(closest - target))
-                        closest = sum;
-                }
-                if (right > j) {
-                    int sum = nums[i] + nums[j] + nums[right];
-                    if (Math.abs(sum - target) < Math.abs(closest - target))
-                        closest = sum;
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum;
                 }
             }
         }

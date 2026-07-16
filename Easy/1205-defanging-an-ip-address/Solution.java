8302d0c706745e92417f88e7ae035a3aace32594
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-16 18:10:24 +0530

    ✅ [Easy] #1205 - Defanging an IP Address | Java | Runtime: 1 ms | Memory: 43 MB

--- Easy/1205-defanging-an-ip-address/Solution.java
+++ Easy/1205-defanging-an-ip-address/Solution.java
@@ -1,13 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String ans = "";
-        for(int i = 0; i < address.length(); i++){
-            if(address.charAt(i) == '.'){
-                ans += "[.]";
-            }else{
-                ans += address.substring(i,i+1);
-            }
-        }
-        return ans;
+        return address.replaceAll("\\.", "[.]");
     }
 }

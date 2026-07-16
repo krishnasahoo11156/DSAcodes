0b7f5a7d161a218d3da880168c1000741dca4bcc
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-16 20:58:48 +0530

    ✅ [Easy] #1651 - Shuffle String | Java | Runtime: 7 ms | Memory: 46.3 MB

--- Easy/1651-shuffle-string/Solution.java
+++ Easy/1651-shuffle-string/Solution.java
@@ -1,9 +1,14 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        StringBuilder sb = new StringBuilder(s);
+        List<int[]> pairs = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            sb.setCharAt(indices[i], s.charAt(i));
+            pairs.add(new int[]{indices[i], s.charAt(i)});
         }
-        return sb.toString();
+        Collections.sort(pairs, (a, b) -> a[0] - b[0]);
+        StringBuilder ans = new StringBuilder();
+        for (int[] pair : pairs) {
+            ans.append((char) pair[1]);
+        }
+        return ans.toString();
     }
 }

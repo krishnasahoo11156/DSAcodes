fd2869b5b108dc5c6f95f403bfd21c6290467957
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-17 15:06:30 +0530

    ✅ [Easy] #1797 - Goal Parser Interpretation | Java | Runtime: 0 ms | Memory: 43 MB

--- Easy/1797-goal-parser-interpretation/Solution.java
+++ Easy/1797-goal-parser-interpretation/Solution.java
@@ -1,7 +1,18 @@
 class Solution {
     public String interpret(String command) {
-        command = command.replace("()", "o");
-        command = command.replace("(al)", "al");
-        return command;
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i<command.length(); i++){
+            if(command.charAt(i) == 'G'){
+                ans.append('G');
+            }else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+                ans.append('o');
+                i++;
+            }else if(command.charAt(i) == '(' && command.charAt(i+1) == 'a'){
+                ans.append('a');
+                ans.append('l');
+                i+=3;
+            }
+        }
+        return ans.toString();
     }
 }

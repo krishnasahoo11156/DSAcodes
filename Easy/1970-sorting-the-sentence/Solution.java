642af3be610ab5a5a22d05692d2cf4137f9f0186
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-17 16:50:38 +0530

    ✅ [Easy] #1970 - Sorting the Sentence | Java | Runtime: 0 ms | Memory: 43.1 MB

--- Easy/1970-sorting-the-sentence/Solution.java
+++ Easy/1970-sorting-the-sentence/Solution.java
@@ -1,14 +1,20 @@
 class Solution {
     public String sortSentence(String s) {
-        String[] words = s.split(" ");
-        String[] ans = new String[words.length];
-        for (String word : words) {
-            int index = word.charAt(word.length() - 1) - '1';
-            ans[index] = word.substring(0, word.length() - 1);
+        String[] ans = new String[9];
+        int start = 0;
+        for (int i = 0; i <= s.length(); i++) {
+            if (i == s.length() || s.charAt(i) == ' ') {
+                String word = s.substring(start, i);
+                int index = word.charAt(word.length() - 1) - '1';
+                ans[index] = word.substring(0, word.length() - 1);
+                start = i + 1;
+            }
         }
         StringBuilder res = new StringBuilder();
         for (String word : ans) {
-            res.append(word).append(" ");
+            if (word != null) {
+                res.append(word).append(" ");
+            }
         }
         res.deleteCharAt(res.length() - 1);
         return res.toString();

232da9d9a49f0327f2f025db43580bb1deba5589
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-18 17:52:38 +0530

    ✅ [Easy] #1434 - Decrypt String from Alphabet to Integer Mapping | Java | Runtime: 1 ms | Memory: 42.9 MB

--- Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/Solution.java
+++ Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/Solution.java
@@ -3,13 +3,12 @@ class Solution{
         StringBuilder ans = new StringBuilder();
         int i = 0;
         while(i < s.length()){
-            if(i + 2 < s.length() && s.charAt(i + 2) == '#'){
-                int num = Integer.parseInt(s.substring(i, i + 2));
-                ans.append((char)(num + 'a' - 1));
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#'){
+                int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
+                ans.append((char)('a' + num - 1));
                 i += 3;
             }else{
-                int num = s.charAt(i) - '0';
-                ans.append((char)(num + 'a' - 1));
+                ans.append((char)('a' + (s.charAt(i) - '0') - 1));
                 i++;
             }
         }

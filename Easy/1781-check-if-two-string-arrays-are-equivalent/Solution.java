07023e84fd73f96d74b02670ba0bf860bcae3308
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-18 15:12:47 +0530

    ✅ [Easy] #1781 - Check If Two String Arrays are Equivalent | Java | Runtime: 6 ms | Memory: 43.7 MB

--- Easy/1781-check-if-two-string-arrays-are-equivalent/Solution.java
+++ Easy/1781-check-if-two-string-arrays-are-equivalent/Solution.java
@@ -1,5 +1,17 @@
-class Solution {
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+class Solution{
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2){
+        List<Character> list1 = new ArrayList<>();
+        List<Character> list2 = new ArrayList<>();
+        for(int i = 0; i < word1.length; i++){
+            for(int j = 0; j < word1[i].length(); j++){
+                list1.add(word1[i].charAt(j));
+            }
+        }
+        for(int i = 0; i < word2.length; i++){
+            for(int j = 0; j < word2[i].length(); j++){
+                list1.add(word2[i].charAt(j));
+            }
+        }
         return String.join("", word1).equals(String.join("", word2));
     }
 }

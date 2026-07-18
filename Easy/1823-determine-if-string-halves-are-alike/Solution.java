2f733c9c1507d488cc8fb4947fd564801371f2fd
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-18 17:13:05 +0530

    ✅ [Easy] #1823 - Determine if String Halves Are Alike | Java | Runtime: 5 ms | Memory: 43.2 MB

--- Easy/1823-determine-if-string-halves-are-alike/Solution.java
+++ Easy/1823-determine-if-string-halves-are-alike/Solution.java
@@ -1,21 +1,17 @@
 class Solution {
+    public boolean isVowel(char ch){
+        return "aeiouAEIOU".indexOf(ch) != -1;
+    }
     public boolean halvesAreAlike(String s) {
-        int a = 0;
-        int b = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(i < s.length()/2){
-                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
-                    a++;
-                }   
-            }else{
-                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
-                    b++;
-                }
+        int count = 0;
+        for(int i = 0; i < s.length()/2; i++){
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+            if(isVowel(s.charAt(i + s.length()/2))){
+                count--;
             }
         }
-        if(a == b){
-            return true;
-        }
-        return false;
+        return count == 0;
     }
 }

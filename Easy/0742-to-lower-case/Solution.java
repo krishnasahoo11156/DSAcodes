5dfb06a1d7b8f668369c3108850e93d036cc6af2
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-18 15:29:44 +0530

    ✅ [Easy] #0742 - To Lower Case | Java | Runtime: 0 ms | Memory: 42.8 MB

--- Easy/0742-to-lower-case/Solution.java
+++ Easy/0742-to-lower-case/Solution.java
@@ -1,6 +1,11 @@
-class Solution {
-    public String toLowerCase(String s) {
-        String ans = s.toLowerCase();
-        return ans;
+class Solution{
+    public String toLowerCase(String s){
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] >= 'A' && arr[i] <= 'Z'){
+                arr[i] = (char)(arr[i] + 32);
+            }
+        }
+        return new String(arr);
     }
 }

92955798bd878501f58c28ffa304f3478ca0a71a
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-19 14:18:07 +0530

    ✅ [Easy] #0657 - Robot Return to Origin | Java | Runtime: 6 ms | Memory: 45.3 MB

--- Easy/0657-robot-return-to-origin/Solution.java
+++ Easy/0657-robot-return-to-origin/Solution.java
@@ -1,31 +1,23 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int vertical = 0; 
+        int vertical = 0;
         int horizontal = 0;
-        int hr = moves.indexOf('R');
-        while(hr != -1){
-            horizontal++;
-            hr = moves.indexOf('R', hr + 1);
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)) {
+                case 'U':
+                    vertical++;
+                    break;
+                case 'D':
+                    vertical--;
+                    break;
+                case 'R':
+                    horizontal++;
+                    break;
+                case 'L':
+                    horizontal--;
+                    break;
+            }
         }
-
-        int hl = moves.indexOf('L');
-        while(hl != -1){
-            horizontal--;
-            hl = moves.indexOf('L', hl + 1);
-        }
-
-        int vu = moves.indexOf('U');
-        while(vu != -1){
-            vertical++;
-            vu = moves.indexOf('U', vu + 1);
-        }
-
-        int vd = moves.indexOf('D');
-        while(vd != -1){
-            vertical--;
-            vd = moves.indexOf('D', vd + 1);
-        }
-        
-        return (vertical == 0 && horizontal == 0);
+        return (horizontal == 0 && vertical == 0);
     }
 }

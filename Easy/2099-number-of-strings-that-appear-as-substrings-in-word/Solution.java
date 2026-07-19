457271765e690f5570688791a7c9697e40cc348b
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-19 08:19:27 +0530

    ✅ [Easy] #2099 - Number of Strings That Appear as Substrings in Word | Java | Runtime: 0 ms | Memory: 43.5 MB

--- Easy/2099-number-of-strings-that-appear-as-substrings-in-word/Solution.java
+++ Easy/2099-number-of-strings-that-appear-as-substrings-in-word/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int numOfStrings(String[] patterns, String word) {
         int count = 0;
         for(int i = 0; i < patterns.length; i++){
-            if(word.contains(patterns[i])){
+            if(word.indexOf(patterns[i]) != -1){
                 count++;
             }
         }

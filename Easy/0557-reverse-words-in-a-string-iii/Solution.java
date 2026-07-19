00ba403b4dcad6019d261405e5ac4d95dbd32b66
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-19 16:13:12 +0530

    ✅ [Easy] #0557 - Reverse Words in a String III | Java | Runtime: 3 ms | Memory: 46.5 MB

---
+++ Easy/0557-reverse-words-in-a-string-iii/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == ' ') {
+                reverse(arr, left, right - 1);
+                left = right + 1;
+            }
+        }
+        reverse(arr, left, arr.length - 1);
+        return new String(arr);
+    }
+    private void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}

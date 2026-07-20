e65070241556744c631621da3e6dbf11dfeaebe2
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-20 21:31:46 +0530

    ✅ [Medium] #0024 - Swap Nodes in Pairs | Java | Runtime: 0 ms | Memory: 42.6 MB

--- Medium/0024-swap-nodes-in-pairs/Solution.java
+++ Medium/0024-swap-nodes-in-pairs/Solution.java
@@ -2,9 +2,19 @@ class Solution {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null)
             return head;
-        ListNode second = head.next;
-        head.next = swapPairs(second.next);
-        second.next = head;
-        return second;
+        ListNode newHead = head.next;
+        head.next = newHead.next;
+        newHead.next = head;
+        ListNode prev = head;
+        ListNode curr = head.next;
+        while (curr != null && curr.next != null) {
+            ListNode second = curr.next;
+            curr.next = second.next;
+            second.next = curr;
+            prev.next = second;
+            prev = curr;
+            curr = curr.next;
+        }
+        return newHead;
     }
 }

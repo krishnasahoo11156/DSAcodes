16a7a552165946570d3c67d07cfbbe2a0d0f0545
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-20 22:13:51 +0530

    ✅ [Easy] #0021 - Merge Two Sorted Lists | Java | Runtime: 0 ms | Memory: 44.5 MB

--- Easy/0021-merge-two-sorted-lists/Solution.java
+++ Easy/0021-merge-two-sorted-lists/Solution.java
@@ -1,18 +1,20 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head;
+
+        if (list1.val <= list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+
+        ListNode tail = head;
 
         while (list1 != null && list2 != null) {
 
@@ -27,11 +29,8 @@ class Solution {
             tail = tail.next;
         }
 
-        if (list1 != null)
-            tail.next = list1;
-        else
-            tail.next = list2;
+        tail.next = (list1 != null) ? list1 : list2;
 
-        return dummy.next;
+        return head;
     }
 }

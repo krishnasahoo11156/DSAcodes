7d69cfaba7eed859d3065d2a2a6bef69a4cc3a3c
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-21 06:48:52 +0530

    ✅ [Easy] #0168 - Excel Sheet Column Title | Java | Runtime: 3 ms | Memory: 42.6 MB

--- Easy/0168-excel-sheet-column-title/Solution.java
+++ Easy/0168-excel-sheet-column-title/Solution.java
@@ -1,11 +1,8 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        StringBuilder sb = new StringBuilder();
-        while (columnNumber > 0) {
+        if (columnNumber == 0)
+            return "";
             columnNumber--;
-            sb.append((char) ('A' + columnNumber % 26));
-            columnNumber /= 26;
-        }
-        return sb.reverse().toString();
+        return convertToTitle(columnNumber / 26) + (char) ('A' + columnNumber % 26);
     }
 }

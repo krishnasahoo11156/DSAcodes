13b53f4d8bd84360d5876865acdbd75b7e03ef44
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-22 12:52:45 +0530

    ✅ [Medium] #0096 - Unique Binary Search Trees | Java | Runtime: 0 ms | Memory: 42 MB

--- Medium/0096-unique-binary-search-trees/Solution.java
+++ Medium/0096-unique-binary-search-trees/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numTrees(int n) {
-        long res=1;
-        for(int i=1;i<=n;i++){
-            res=res*(n+i)/i;
+        long ans = 1;
+        for (int i = 0; i < n; i++) {
+            ans = ans * (2L * n - i) / (i + 1);
         }
-        return (int)(res/(n+1));
+        return (int)(ans / (n + 1));
     }
 }

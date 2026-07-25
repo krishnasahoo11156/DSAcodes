4fae047a880e156cc65c3211a36e3c69eb2d8fb2
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-25 22:32:13 +0530

    ✅ [Easy] #0263 - Ugly Number | Java | Runtime: 1 ms | Memory: 42.7 MB

--- Easy/0263-ugly-number/Solution.java
+++ Easy/0263-ugly-number/Solution.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean isUgly(int n) {
 
-        if (n == 1) return true;
         if (n <= 0) return false;
 
-        if (n % 2 == 0) return isUgly(n / 2);
-        if (n % 3 == 0) return isUgly(n / 3);
-        if (n % 5 == 0) return isUgly(n / 5);
+        while (n % 2 == 0) {
+            n /= 2;
+        }
 
-        return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+
+        return n == 1;
     }
 }

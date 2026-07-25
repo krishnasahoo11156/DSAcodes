f265a2790594945eb10f388f4e785c459318887f
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-25 22:12:41 +0530

    ✅ [Easy] #0496 - Next Greater Element I | Java | Runtime: 4 ms | Memory: 45.2 MB

--- Easy/0496-next-greater-element-i/Solution.java
+++ Easy/0496-next-greater-element-i/Solution.java
@@ -1,24 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+
+            stack.push(nums2[i]);
+        }
+
         int[] ans = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
-            int idx = -1;
-
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    idx = j;
-                    break;
-                }
-            }
-
-            ans[i] = -1;
-            for (int j = idx + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    ans[i] = nums2[j];
-                    break;
-                }
-            }
+            ans[i] = map.get(nums1[i]);
         }
 
         return ans;

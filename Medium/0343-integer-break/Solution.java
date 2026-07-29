1f29ef819a23fa1e4e0acd3e39b479b91de7ecba
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-29 21:15:43 +0530

    ✅ [Medium] #0343 - Integer Break | Java | Runtime: 0 ms | Memory: 41.9 MB

--- Medium/0343-integer-break/Solution.java
+++ Medium/0343-integer-break/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int integerBreak(int n) {
-        int[] dp = new int[n + 1];
-        dp[2] = 1;
-        for(int i = 3; i <= n; i++){
-            for(int j = 1; j < i; j++){
-                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
-            }
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        int ans = 1;
+        while(n > 4){
+            ans *= 3;
+            n -= 3;
         }
-        return dp[n];
+        return ans * n;
     }
 }

aaf6d723338b34608c09f17e9cf4de9f4336d380
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-07-30 19:06:09 +0530

    ✅ [Medium] #1080 - Camelcase Matching | Java | Runtime: 9 ms | Memory: 45.6 MB

--- Medium/1080-camelcase-matching/Solution.java
+++ Medium/1080-camelcase-matching/Solution.java
@@ -1,24 +1,14 @@
 class Solution {
     public List<Boolean> camelMatch(String[] queries, String pattern) {
         List<Boolean> ans = new ArrayList<>();
+        StringBuilder regex = new StringBuilder("^[a-z]*");
+        for(char c : pattern.toCharArray()){
+            regex.append(c).append("[a-z]*");
+        }
+        regex.append("$");
         for(String query : queries){
-            ans.add(match(query, pattern));
+            ans.add(query.matches(regex.toString()));
         }
         return ans;
-    }
-
-    public boolean match(String query, String pattern){
-        int i = 0, j = 0;
-        while(i < query.length()){
-            if(j < pattern.length() && query.charAt(i) == pattern.charAt(j)){
-                i++;
-                j++;
-            }else if(Character.isLowerCase(query.charAt(i))){
-                i++;
-            }else{
-                return false;
-            }
-        }
-        return j == pattern.length();
     }
 }

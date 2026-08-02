0bef461e584c9fd684ec712240bcc7637dc24cb2
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-08-02 18:29:11 +0530

    ✅ [Easy] #1319 - Unique Number of Occurrences | Java | Runtime: 5 ms | Memory: 43.5 MB

--- Easy/1319-unique-number-of-occurrences/Solution.java
+++ Easy/1319-unique-number-of-occurrences/Solution.java
@@ -1,25 +1,18 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-
         Arrays.sort(arr);
-
         HashSet<Integer> set = new HashSet<>();
-
         int count = 1;
-
         for (int i = 1; i < arr.length; i++) {
-
             if (arr[i] == arr[i - 1]) {
                 count++;
             } else {
-
                 if (!set.add(count))
                     return false;
 
                 count = 1;
             }
         }
-
         return set.add(count);
     }
 }
